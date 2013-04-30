1ae7909df9a3341116eca366a81284aee2788933
Evan Dana <evan.dana@gmail.com>
2013-04-29 20:31:28 -0400

    temp commits
    working on fleshing out some todos for solo/duo

--- src/com/example/duodeck/Game.java
+++ src/com/example/duodeck/Game.java
@@ -16,9 +16,18 @@ public class Game extends Activity {
 //	public String suit = null; // d|c|h|s per above
 	
 	public Deck deck = new Deck();
-	
 
-	
+	private final String NoGame = "NoGame";
+	private final String Solo = "Solo";
+	private final String MeInvitingBuddy = "MeInvitingBuddy";
+	private final String BuddyInvitingMe = "BuddyInvitingMe";
+	private final String BothWorkingout = "BothWorkingout";
+	private final String MeWorkingOutAndBuddyWaiting = "MeWorkingOutAndBuddyWaiting";
+	private final String MeWaitingAndBuddyWorkingOut = "MeWaitingAndBuddyWorkingOut";
+	private final String BothDone = "BothDone";
+	// TODO: finish this and use ones from Nirmal's suggestions
+	private String gameState = NoGame;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,20 +39,51 @@ public class Game extends Activity {
 		
 		TextView deckInfo = (TextView) findViewById(R.id.display_deck_info);
 		deckInfo.setText(deck.showDeck());
-		
 	}
 	
 	public void doneWithThisCard(View view) 
 	{
-		System.out.println("done with this card");
-
-		TextView deckInfo = (TextView) findViewById(R.id.display_deck_info);
-		deckInfo.setText(deck.showDeck());
-		
-		TextView currentCard = (TextView) findViewById(R.id.display_current_card);
-		currentCard.setText(deck.drawFromDeck() + "\t\t" + deck.getCardsRemaining() + "/" + deck.getDeckSize() );
-
+		if (gameState == Solo) {
+			// record the time that the card was finished
+			
+			if (deck.getCardsRemaining() > 0) 
+			{
+				// get new card
+				// remove the card from the deck of available options
+				// display the next card
+				System.out.println("done with this card");
+	
+				TextView deckInfo = (TextView) findViewById(R.id.display_deck_info);
+				deckInfo.setText(deck.showDeck());
+				
+				TextView currentCard = (TextView) findViewById(R.id.display_current_card);
+				currentCard.setText(deck.drawFromDeck() + "\t\t" + deck.getCardsRemaining() + "/" + deck.getDeckSize() );
+			} else {
+				// TODO: record finished time
+				// TODO: record deck stats in stats
+				// TODO: show stats
+				// TODO: show finished time
+				// TODO: show summary of deck stats (sum of values per suit)
+				// TODO: show summary of deck stats (sum of values per EXERCISE)
+			}
+			
+			
+		} else {
+			if (gameState == MeWaitingAndBuddyWorkingOut) {
+				// TODO: record the time that this was finished
+				// TODO: queue a new card (pull it from the deck), but don't show it yet
+				// TODO: Notify partner that i'm done and send the card
+				// TODO: show "wait" screen
+				// TODO: put in "wait" state that can receive a push notification to move forward
+			} else if (gameState == MeWorkingOutAndBuddyWaiting) {
+				// TODO: request the card from buddy
+				// TODO: remove this card from my deck
+				// TODO: Notify partner that i'm done
+				// TODO: start the next card
+			}
+		}
 	}
+	
 	
 	
 	
